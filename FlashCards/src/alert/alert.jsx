abf73e0aac4cfb53d21f9c7f8e182c63f22147ed
sc-forlife <salemsc7@gmail.com>
2026-04-13 08:57:17 +0200

    Modified Alert For Various Functions

--- FlashCards/src/alert/alert.jsx
+++ FlashCards/src/alert/alert.jsx
@@ -4,16 +4,39 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCircle,
   faCircleInfo,
+  faCircleQuestion,
   faHome,
+  faTriangleExclamation,
 } from "@fortawesome/free-solid-svg-icons";
 
 export default function alert({ message = "Hello World" }) {
+  const [isWarning, setIsWarning] = useState(false);
+  const [isQuestion, setIsQuestion] = useState(false);
+  const [isInformation, setIsInformation] = useState(false);
+
   return (
     <>
-      <div className={css.alertContainer}>
-        <h1>{message}</h1>
-        <FontAwesomeIcon className={css.iconSize} icon={faCircleInfo} />
-      </div>
+      {isWarning ? (
+        <div className={css.alertContainer}>
+          <h1>{message}</h1>
+          <FontAwesomeIcon
+            className={css.iconSize}
+            icon={faTriangleExclamation}
+          />
+        </div>
+      ) : isQuestion ? (
+        <div className={css.alertContainer}>
+          <h1>{message}</h1>
+          <FontAwesomeIcon className={css.iconSize} icon={faCircleQuestion} />
+        </div>
+      ) : isInformation ? (
+        <div className={css.alertContainer}>
+          <h1>{message}</h1>
+          <FontAwesomeIcon className={css.iconSize} icon={faCircleInfo} />
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </>
   );
 }
